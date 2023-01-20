4328aab4d229e026e34bcae3153d470b11e7f65f
suentze2020 <suentze2020@gmail.com>
2023-01-20 10:07:38 +0800

    mdl pwa v2
    mdl pwa v2

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("./service_worker.js") // .register("./sw_cache_pages.js")
+      .register("/sw.js") // .register("./sw_cache_pages.js")
       .then((reg) => console.log("Service Worder: Registered"))
       .catch((err) => console.log(`Service Worker: Error: ${err}`));
   });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "v1";
+const cacheName = "v2";
 
 // Call install event
 self.addEventListener("install", (e) => {
